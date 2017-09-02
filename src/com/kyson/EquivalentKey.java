1a383ee1c3ec93fc9f3303ca6c0f6de00d3f9ba4
zjh171 <jinhui.zhu@medishare.cn>
2017-09-03 00:12:23 +0800

    src/com/kyson/EquivalentKey.java

--- src/com/kyson/EquivalentKey.java
+++ src/com/kyson/EquivalentKey.java
@@ -1,5 +1,6 @@
 package com.kyson;
 
+import java.nio.file.SimpleFileVisitor;
 
 /**
  * 等值键:http://www.jianshu.com/p/a1a9c6b0254c
@@ -17,6 +18,67 @@ public class EquivalentKey {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		int[] N = {1,2,3,4,5,5,5,6,7,8,9,89,89,100};
+		
+		System.out.println(rank(5,N) + "");
+		System.out.println(count(5,N) +"");
+		
+	}
+	
+	public static int binarySearch(int target ,int[] N) {
+		int lo = 0;
+		int hi = N.length - 1;
+		
+		while (lo < hi) {
+			int mid = lo + (hi - lo) / 2 ;
+
+			if (target > N[mid]) {
+				lo = mid + 1 ;
+			} else if (target < N[mid]) {
+				hi = mid - 1;
+			} else {
+				 return mid;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public static int rank(int key,int[] N){
+	
+		int searchResult = binarySearch(key, N);
+		System.out.println("searchResult: " + searchResult);
+		
+		int resultNum = 0;
+		
+		if (searchResult != -1) {
+			for (int i = 0; i < N.length; i++) {
+				if (N[i] == N[searchResult] ) {
+					resultNum = i;
+					break;
+				}
+			}			
+		}
+		
+		return resultNum;
+	}
+	
+	public static int count(int key,int[] N) {
+		int searchResult = binarySearch(key, N);
+		System.out.println("searchResult: " + searchResult);
+
+		int resultNum = 0;
+
+		if (searchResult != -1) {
+			for (int i = 0; i < N.length; i++) {
+				if (N[i] == N[searchResult] ) {
+					resultNum++;
+				}
+			}			
+		}
+		
+		return resultNum;
 	}
 
 }
