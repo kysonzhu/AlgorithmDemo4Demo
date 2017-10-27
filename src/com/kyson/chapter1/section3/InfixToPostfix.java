01a3356de36eb95da034d1b99c2991dd740ae431
zhujinhui <zhujinhui@imdada.cn>
2017-10-27 17:01:46 +0800

    [update]infixtopostfix

--- src/com/kyson/chapter1/section3/InfixToPostfix.java
+++ src/com/kyson/chapter1/section3/InfixToPostfix.java
@@ -1,5 +1,7 @@
 package com.kyson.chapter1.section3;
 
+import java.util.Scanner;
+
 /***
  * 1.3.10 编写一个过滤器InfixToPostfix，将算术表达式由中序表达式转为后序表达式。 1.3.10 Write a filter
  * InfixToPostfix that converts an arithmetic expression from infix to postfix.
@@ -7,47 +9,135 @@ package com.kyson.chapter1.section3;
  * 
  */
 public class InfixToPostfix {
-	
 
+	/*******************************************************/
+	// public static void main(String[] args) {
+	// // 双栈
+	// String expression = "(1+((2+3)*(4*5)))";
+	//
+	// Stack<String> ops = new Stack<String>();
+	// Stack<Double> vals = new Stack<Double>();
+	// double result = 0;
+	// for (int j = 0; j < expression.length(); j++) {
+	// char charAtIndex = expression.charAt(j);
+	// String s = String.valueOf(charAtIndex);
+	// if(s.equals("("))
+	// {
+	// ;
+	// }
+	// else if (s.equals("+"))
+	// {
+	// ops.push(s);
+	// }
+	// else if (s.equals("-"))
+	// {
+	// ops.push(s);
+	// }
+	// else if (s.equals("*"))
+	// {
+	// ops.push(s);
+	// }
+	// else if (s.equals("/"))
+	// {
+	// ops.push(s);
+	// }
+	// else if (s.equals(")")){
+	// String op = ops.pop();
+	// double v = vals.pop();
+	// if (op.equals("+")) {
+	// v = v + vals.pop();
+	// }else if (op.equals("-")) {
+	// v = v - vals.pop();
+	// }else if (op.equals("*")) {
+	// v = v * vals.pop();
+	// }else if (op.equals("/")) {
+	// v = v / vals.pop();
+	// }
+	// vals.push(v);
+	// }else {
+	// vals.push(Double.parseDouble(s));
+	// }
+	//
+	// }
+	// System.out.println("result :" + vals.pop() + "");
+	//
+	//
+	// }
+
+	/*********************************************/
 	public static void main(String[] args) {
 		// 双栈
 		String expression = "(1+((2+3)*(4*5)))";
 
 		Stack<String> ops = new Stack<String>();
 		Stack<Double> vals = new Stack<Double>();
-		
+		double result = 0;
 		for (int j = 0; j < expression.length(); j++) {
 			char charAtIndex = expression.charAt(j);
 			String s = String.valueOf(charAtIndex);
-			if(s.equals("(")) 
-			{
-			;
-			}
-			else if (s.equals("+"))
-			{
+			if (s.equals("(")) {
+				;
+			} else if (s.equals("+")) {
 				ops.push(s);
-			}
-			else if (s.equals("-"))
-			{
+			} else if (s.equals("-")) {
 				ops.push(s);
-			}
-			else if (s.equals("*"))
-			{
+			} else if (s.equals("*")) {
 				ops.push(s);
-			}
-			else if (s.equals("/"))
-			{
+			} else if (s.equals("/")) {
 				ops.push(s);
-			}
-			else if (s.equals(")")){
+			} else if (s.equals(")")) {
 				String op = ops.pop();
-	            double v = vals.pop();
+				System.out.print(op);
+				// double v = vals.pop();
+				// if (op.equals("+")) {
+				// v = v + vals.pop();
+				// }else if (op.equals("-")) {
+				// v = v - vals.pop();
+				// }else if (op.equals("*")) {
+				// v = v * vals.pop();
+				// }else if (op.equals("/")) {
+				// v = v / vals.pop();
+				// }
+				// vals.push(v);
+			} else {
+				vals.push(Double.parseDouble(s));
+				System.out.print(s);
 			}
-
 
 		}
-		
+		// System.out.println("result :" + vals.pop() + "");
 
-		
 	}
+
+	/*******************************************************/
+
+//	public static void main(String[] args) {
+//		// 双栈
+//		String expression = "(1+((2+3)*(4*5)))";
+//
+//		Stack<String> ops = new Stack<String>();
+//		double result = 0;
+//		for (int j = 0; j < expression.length(); j++) {
+//			char charAtIndex = expression.charAt(j);
+//			String s = String.valueOf(charAtIndex);
+//			if (s.equals("(")) {
+//				;
+//			} else if (s.equals("+")) {
+//				ops.push(s);
+//			} else if (s.equals("-")) {
+//				ops.push(s);
+//			} else if (s.equals("*")) {
+//				ops.push(s);
+//			} else if (s.equals("/")) {
+//				ops.push(s);
+//			} else if (s.equals(")")) {
+//				String op = ops.pop();
+//				System.out.print(op);
+//			} else {
+//				System.out.print(s);
+//			}
+//
+//		}
+//
+//	}
 }
