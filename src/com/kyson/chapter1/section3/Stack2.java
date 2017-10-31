22441f704b15e66b901618736c31197482125d4f
kyson <kyson@kysondeMacBook-Air.local>
2017-10-31 23:56:21 +0800

    [add]QueueExecise1.java

--- src/com/kyson/chapter1/section3/Stack2.java
+++ src/com/kyson/chapter1/section3/Stack2.java
@@ -9,7 +9,7 @@ import java.util.Iterator;
  * 1.3.12 Write an iterable Stack client that has a static method copy() that
  * takes a stack of strings as argument and returns a copy of the stack. Note:
  * This ability is a prime example of the value of having an iterator, because
- * it allows development of such func- tionality without changing the basic API.
+ * it allows development of such functionality without changing the basic API.
  */
 public class Stack2<Item> implements Iterable<Item> {
 
