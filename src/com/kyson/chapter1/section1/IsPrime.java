567600173a2e40871ed00626fb3a7aa6ab877095
zhujinhui <zhujinhui@imdada.cn>
2017-11-07 13:52:22 +0800

    [update]src/com/kyson/chapter1/section1/IsPrime.java

--- src/com/kyson/chapter1/section1/IsPrime.java
+++ src/com/kyson/chapter1/section1/IsPrime.java
@@ -8,7 +8,8 @@ public class IsPrime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		boolean is = isPrime4(5);
+		System.out.println(is +"-----");
 	}
 
 	/***
@@ -48,7 +49,22 @@ public class IsPrime {
 				return false;
 		return true;
 	}
-	
-	
+
+	public static boolean isPrime4(int num) {
+		// 两个较小数另外处理
+		if (num == 2 || num == 3)
+			return true;
+		// 不在6的倍数两侧的一定不是质数
+		if (num % 6 != 1 && num % 6 != 5)
+			return false;
+		int tmp = (int) Math.sqrt(num);
+		// 在6的倍数两侧的也可能不是质数
+		for (int i = 5; i <= tmp; i += 6)
+			if (num % i == 0 || num % (i + 2) == 0)
+				return false;
+		// 排除所有，剩余的是质数
+		return true;
+	}
 
 }
+
