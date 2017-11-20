886a582da2361a2fd8ea2d5af122dcd8ad9735e9
kyson <kyson@kysondeMacBook-Air.local>
2017-11-20 11:44:25 +0800

    [add]randomqueue

--- src/com/kyson/chapter1/section3/RandomQueue.java
+++ src/com/kyson/chapter1/section3/RandomQueue.java
@@ -1,5 +1,7 @@
 package com.kyson.chapter1.section3;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  * 1.3.35 随机队列。随机队列能够存储一组元素并支持下表中的 API。 编写一个 RandomQueue 类来实现这份 API。编写一个用例，使用
  * RandomQueue 在桥牌中发牌。 1.3.35 Random queue. A random queue stores a collection
@@ -13,34 +15,60 @@ package com.kyson.chapter1.section3;
  */
 public class RandomQueue<Item> {
 
+	private Item[] a;
+	private int N;
+	
+	public RandomQueue(){
+		a =(Item[])(new Object[1]);
+		N = 0;
+	}
+	
 	public boolean isEmpty() {
-		return true;
+		return N == 0;
 	}
 
 	public int size() {
-		return 0;
+		return N;
 	}
 
 	public void enqueue(Item x) {
-
+		if (N == a.length) {
+			this.resize(N * 2);
+		}
+		a[N++] = x;
 	}
 	
 	public Item dequeue(){
-		
-		return null;
+		if (this.isEmpty()) {
+			return null;
+		}
+		StdRandom.shuffle(a);
+		if (N == a.length / 4) {
+			resize(N / 2);
+		}
+		Item x = a[--N];
+		a[N] = null;
+		return x;
 	}
 
 	
-	public void resize(){
-		
+	public void resize(int max){
+		Item[] temp = (Item[])new Object[max];
+		for (int i = 0; i < N; i++) {
+			temp[i] = a[i];
+		}
+		a = temp;
 	}
 	
 	public Item sample(){
-		return null;
+		if (this.isEmpty()) {
+			return null;
+		}
+		StdRandom.shuffle(a);
+		return a[0];
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
