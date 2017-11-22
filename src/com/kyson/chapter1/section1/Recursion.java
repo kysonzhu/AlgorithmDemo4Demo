1fc9f11ddeb5ed0087ed646d12887d1491454860
kyson <kyson@kysondeMacBook-Air.local>
2017-11-22 23:02:55 +0800

    [update]Recursion.java

--- src/com/kyson/chapter1/section1/Recursion.java
+++ src/com/kyson/chapter1/section1/Recursion.java
@@ -11,32 +11,6 @@ import edu.princeton.cs.algs4.StdOut;
 public class Recursion {
 
 	/**
-	 * 
-	 * 1.1.15 编写一个静态方法 histogram()，接受一个整型数组 a[] 和一个整数 M 为参数并返回一个大小为 M 的数组，其中第 i
-	 * 个元素的值为整数 i 在参数数组中出现的次数。如果 a[] 中的值均在 0 到 M-1之间，返回数组中所有元素之和应该和 a.length 相等。
-	 */
-	public static int[] histogram(int[] a, int M) {
-		int[] b = new int[M];
-		int n = 0;
-		int m = 0;
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < a.length; j++) {
-				if (i == a[j]) {
-					n++;
-				}
-				b[i] = n;
-			}
-			n = 0;
-		}
-
-		for (int i = 0; i < M; i++) {
-			m = m + b[i];
-		}
-
-		return b;
-	}
-
-	/**
 	 * 1.1.16 给出 exR1(6) 的返回值:
 	 */
 	public static String exR1(int n) {
