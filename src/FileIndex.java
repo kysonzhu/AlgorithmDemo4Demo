60b24d2fcba4d5603f78767ec2491327a764172d
kyson <kyson@kysondeMacBook-Air.local>
2017-11-26 09:47:19 +0800

    [update]

--- src/FileIndex.java
+++ src/FileIndex.java
@@ -39,69 +39,75 @@ import com.kyson.chapter1.section3.RingBuffer;
 import com.kyson.chapter1.section3.Stack;
 import com.kyson.chapter1.section3.Stack2;
 import com.kyson.chapter1.section3.Steque;
+import com.kyson.chapter1.section4.ThreeSum;
 
 public class FileIndex {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		HashMap<String, String> map = new HashMap<String, String>();
+        HashMap<String, String> map = new HashMap<String, String>();
 
-		/****
-		 * section 1
-		 */
-		// 找到重复元素
+        /****
+         * section 1
+         */
+        // 找到重复元素
 
-		map.put("1.1.15", HistogramSample.class.getName());
-		map.put("1.1.27", BinomialSample.class.getName());
-		map.put("1.1.28", DumplicateElements.class.getName());
-		map.put("1.1.29", EquivalentKey.class.getName());
-		map.put("1.1.32", Histogram.class.getName());
+        map.put("1.1.15", HistogramSample.class.getName());
+        map.put("1.1.27", BinomialSample.class.getName());
+        map.put("1.1.28", DumplicateElements.class.getName());
+        map.put("1.1.29", EquivalentKey.class.getName());
+        map.put("1.1.32", Histogram.class.getName());
 
-		/***
-		 * section 2
-		 */
-		map.put("1.2.1", Point2D.class.getName());
-		map.put("1.2.2", Interval1D.class.getName());
-		map.put("1.2.3", Interval2D.class.getName());
-		map.put("1.2.6", CircularRotation.class.getName());
-		map.put("1.2.9", BinarySearchCounter.class.getName());
-		map.put("1.2.10", VisualCounter.class.getName());
-		map.put("1.2.11", SmartDate1.class.getName());
-		map.put("1.2.12", SmartDate2.class.getName());
-		map.put("1.2.13", Transaction.class.getName());
-		map.put("1.2.14", Transaction.class.getName());
+        /***
+         * section 2
+         */
+        map.put("1.2.1", Point2D.class.getName());
+        map.put("1.2.2", Interval1D.class.getName());
+        map.put("1.2.3", Interval2D.class.getName());
+        map.put("1.2.6", CircularRotation.class.getName());
+        map.put("1.2.9", BinarySearchCounter.class.getName());
+        map.put("1.2.10", VisualCounter.class.getName());
+        map.put("1.2.11", SmartDate1.class.getName());
+        map.put("1.2.12", SmartDate2.class.getName());
+        map.put("1.2.13", Transaction.class.getName());
+        map.put("1.2.14", Transaction.class.getName());
 
-		/***
-		 * section 3
-		 */
-		map.put("1.3.1", FixedCapacityStackOfStrings.class.getName());
-		map.put("1.3.2", Stack.class.getName());
-		map.put("1.3.4", Parentheses.class.getName());
-		map.put("1.3.8", DoublingStackOfStrings.class.getName());
-		map.put("1.3.10", InfixToPostfix.class.getName());
-		map.put("1.3.11", EvaluatePostfix.class.getName());
-		map.put("1.3.12", Stack2.class.getName());
-		map.put("1.3.14", ResizingArrayQueueOfStrings.class.getName());
-		map.put("1.3.15", QueueExecise1.class.getName());
-		map.put("1.3.16", Date1.class.getName());
-		map.put("1.3.19", LinkedListExecise1.class.getName());
-		map.put("1.3.20", LinkedListExecise2.class.getName());
-		map.put("1.3.21", LinkedListExecise3.class.getName());
-		map.put("1.3.24", LinkedListExecise4.class.getName());
-		map.put("1.3.25", LinkedListExecise5.class.getName());
-		map.put("1.3.26", LinkedListExecise6.class.getName());
-		map.put("1.3.27", LinkedListExecise7.class.getName());
-		map.put("1.3.29", CircularLinkedListQueue.class.getName());
-		map.put("1.3.30", LinkedListExecise8.class.getName());
+        /***
+         * section 3
+         */
+        map.put("1.3.1", FixedCapacityStackOfStrings.class.getName());
+        map.put("1.3.2", Stack.class.getName());
+        map.put("1.3.4", Parentheses.class.getName());
+        map.put("1.3.8", DoublingStackOfStrings.class.getName());
+        map.put("1.3.10", InfixToPostfix.class.getName());
+        map.put("1.3.11", EvaluatePostfix.class.getName());
+        map.put("1.3.12", Stack2.class.getName());
+        map.put("1.3.14", ResizingArrayQueueOfStrings.class.getName());
+        map.put("1.3.15", QueueExecise1.class.getName());
+        map.put("1.3.16", Date1.class.getName());
+        map.put("1.3.19", LinkedListExecise1.class.getName());
+        map.put("1.3.20", LinkedListExecise2.class.getName());
+        map.put("1.3.21", LinkedListExecise3.class.getName());
+        map.put("1.3.24", LinkedListExecise4.class.getName());
+        map.put("1.3.25", LinkedListExecise5.class.getName());
+        map.put("1.3.26", LinkedListExecise6.class.getName());
+        map.put("1.3.27", LinkedListExecise7.class.getName());
+        map.put("1.3.29", CircularLinkedListQueue.class.getName());
+        map.put("1.3.30", LinkedListExecise8.class.getName());
 
-		map.put("1.3.32", Steque.class.getName());
-		map.put("1.3.34", RandomBag.class.getName());
-		map.put("1.3.35", RandomQueue.class.getName());
-		map.put("1.3.37", Josephus.class.getName());
-		map.put("1.3.38", GeneralizedQueue.class.getName());
-		map.put("1.3.39", RingBuffer.class.getName());
+        map.put("1.3.32", Steque.class.getName());
+        map.put("1.3.34", RandomBag.class.getName());
+        map.put("1.3.35", RandomQueue.class.getName());
+        map.put("1.3.37", Josephus.class.getName());
+        map.put("1.3.38", GeneralizedQueue.class.getName());
+        map.put("1.3.39", RingBuffer.class.getName());
 
-	}
+        /***
+         * section 3
+         */
+        map.put("1.4.2", ThreeSum.class.getName());
+
+    }
 
 }
--- src/com/kyson/chapter1/section4/ThreeSum.java
+++ src/com/kyson/chapter1/section4/ThreeSum.java
@@ -3,26 +3,31 @@ package com.kyson.chapter1.section4;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+/**
+ * 1.4.2 修改TreeSum,正确处理两个较大的int值相加可能溢出的情况
+ * <p>
+ * 1.4.2 Modify ThreeSum to work properly even when the int values are so large
+ * that adding two of them might cause overflow.
+ */
 public class ThreeSum {
-	public static int count(long[] a) { // Count triples that sum to 0.
-		int N = a.length;
-		int cnt = 0;
-		for (int i = 0; i < N; i++)
-			for (int j = i + 1; j < N; j++)
-				for (int k = j + 1; k < N; k++)
-					if (a[i] + a[j] + a[k] == 0)
-						cnt++;
-		return cnt;
-	}
+    public static int count(long[] a) { // Count triples that sum to 0.
+        int N = a.length;
+        int cnt = 0;
+        for (int i = 0; i < N; i++)
+            for (int j = i + 1; j < N; j++)
+                for (int k = j + 1; k < N; k++)
+                    if (a[i] + a[j] + a[k] == 0)
+                        cnt++;
+        return cnt;
+    }
 
-	public static void main(String[] args) {
-		String filePathString = System.getProperty("user.dir");
-		String intFileString = filePathString
-				+ "/src/com/kyson/chapter1/section4/" + "5ints.txt";
-		System.out.println("即将读取" + intFileString + "文件中得到的数组为：");
+    public static void main(String[] args) {
+        String filePathString = System.getProperty("user.dir");
+        String intFileString = filePathString
+                + "/src/com/kyson/chapter1/section4/" + "5ints.txt";
 
-		In in = new In(intFileString);
-		long[] a = in.readAllLongs();
-		StdOut.println(count(a));
-	}
+        In in = new In(intFileString);
+        long[] a = in.readAllLongs();
+        StdOut.println(count(a));
+    }
 }
