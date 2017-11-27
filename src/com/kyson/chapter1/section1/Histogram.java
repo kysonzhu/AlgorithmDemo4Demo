56c2a5233b3a8c00fe9c906965a1d5d4f4edcee8
zhujinhui <zhujinhui@imdada.cn>
2017-11-27 21:13:33 +0800

    [update]DoublingTest2.java

--- src/com/kyson/chapter1/section1/Histogram.java
+++ src/com/kyson/chapter1/section1/Histogram.java
@@ -51,20 +51,6 @@ public class Histogram {
 			StdDraw.filledRectangle(tempLeftReact, height, intervalReact,
 					height);
 		}
-
-		/*
-		 * demo 用于展示StdDraw.filledRectangle 的用法
-		 */
-		/*
-		 * int N = 50; double[] a = new double[N]; for (int i = 0; i < N; i++)
-		 * a[i] = StdRandom.random();
-		 * 
-		 * Arrays.sort(a); for (int i = 0; i < N; i++) { double x = 1.0*i/N;
-		 * double y = a[i]/2.0; double rw = 0.5/N; double rh = a[i]/2.0;
-		 * 
-		 * System.out.println("x:"+x+"y:"+y+"rw:"+rw+"rh:"+rh);
-		 * StdDraw.filledRectangle(x, y, rw, rh); }
-		 */
 	}
 
 }
--- src/com/kyson/chapter1/section4/DoublingTest.java
+++ src/com/kyson/chapter1/section4/DoublingTest.java
@@ -16,7 +16,7 @@ public class DoublingTest {
     }
 
     public static void main(String[] args) {  // Print table of running times.
-        for (int N = 250; true; N = 2 * N) {  // Print time for problem size N.
+        for (int N = 250; true; N += N) {  // Print time for problem size N.
             double time = timeTrial(N);
             StdOut.printf("%7d %5.1f\n", N, time);
         }
