d0285de604a074e20d40c945059ac01cd9c9fe59
zhujinhui <zhujinhui@imdada.cn>
2017-12-11 20:23:56 +0800

    [add]two same

---
+++ src/com/kyson/chapter1/section4/TwoSameFast.java
@@ -0,0 +1,38 @@
+package com.kyson.chapter1.section4;
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.In;
+
+/***
+ * 1.4.8
+ * 编写一个程序,计算输入文件中相等的整数对的数量.如果你的第一个程序是平方级别的,请继续思考并以Array.sort()给出一个线性对数级别的解答
+ * 
+ * 1.4.8 Write a program to determine the number pairs of values in an input
+ * file that are equal. If your first try is quadratic, think again and use
+ * Arrays.sort() to develop a linearithmic solution.
+ *
+ */
+
+public class TwoSameFast {
+
+	public static void main(String[] args) {
+		// int[] a = {1,2,3,4,5,6,7,0,7,9};
+		String filePathString = System.getProperty("user.dir");
+		String intFileString = filePathString
+				+ "/src/com/kyson/chapter1/section4/" + "1kints.txt";
+
+		In in = new In(intFileString);
+		long[] a = in.readAllLongs();
+		Arrays.sort(a);
+
+		int cnt = 0;
+		for (int i = 0; i < a.length - 1; i++) 
+		{
+			if (a[i] == a[i+1]) {
+				cnt++;
+			}
+		}
+		System.out.println("相同的整数的数量为" + cnt);
+	}
+}
