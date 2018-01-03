e4c1271e97fb0ac1a9102ecb701d7ddcaf24e754
zhujinhui <zhujinhui@imdada.cn>
2018-01-03 17:25:18 +0800

    [add]MatrixLocalMinimum

---
+++ src/com/kyson/chapter1/section4/MatrixLocalMinimum.java
@@ -0,0 +1,21 @@
+package com.kyson.chapter1.section4;
+
+/**
+ * 
+ * 1.4.19 矩阵的局部最小元素。给定一个含有 N^2 个不同整数的 N×N 数组 a[]。设计一个运送时间和 N
+ * 成正比的算法来找出一个局部最小元素：满足 a[i][j] < a[i+1][j]、a[i][j] < a[i][j+1]、a[i][j] <
+ * a[i-1][j] 以及 a[i][j] < a[i][j-1] 的索引 i 和 j。程序运行时间在最坏情况下应该和 N 成正比。
+ * 
+ * 1.4.19 Local minimum of a matrix. Given an N-by-N array a[] of N 2 distinct
+ * integers, design an algorithm that runs in time proportional to N to find a
+ * local minimum: a pair of indices i and j such that a[i][j] < a[i+1][j],
+ * a[i][j] < a[i][j+1], a[i][j] < a[i-1][j], and a[i][j] < a[i][j-1]. The
+ * running time of your program should be proportional to N in the worst case.
+ */
+public class MatrixLocalMinimum {
+
+	public static void main(String[] args) {
+
+	}
+
+}
