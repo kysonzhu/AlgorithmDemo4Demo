496f3261af88f9fab28ad840143611f81145ef2f
kyson <kyson@kysondeMacBook-Air.local>
2018-01-07 10:35:58 +0800

    [update]fibonacci search

--- src/com/kyson/chapter1/section4/FibonacciSearch.java
+++ src/com/kyson/chapter1/section4/FibonacciSearch.java
@@ -3,10 +3,27 @@ package com.kyson.chapter1.section4;
 import java.util.Arrays;
 
 /**
- *
+ * 1.4.22 仅用加减实现的二分查找（Mihai Patrascu）。编写一个程序，给定一个含有 N 个不同 int
+ * 值的按照升序排列的数组，判断它是否含有给定的整数。只能使用加法和减法以及常数的额外内存空间。
+ * 程序的运行时间在最坏情况下应该和 logN成正比。提示：用斐波那契数代替2的幂（二分法）进行查找。
+ * 用两个变量保存Fk和Fk-1并在[i,i+Fk]之间查找。在每一步中，使用减法计算Fk-2，检查i+Fk-2处的
+ * 元素，并根据结果将搜索范围变为[i,i+Fk-2]或是[i+Fk-2,i+Fk-2+Fk-1]。
+ * <p>
+ * 1.4.22 Binary search with only addition and subtraction. [Mihai Patrascu]
+ * Write a program that, given an array of N distinct int values in ascending
+ * order, determines whether a given integer is in the array. You may use
+ * only additions and subtractions and a constant amount of extra memory. The
+ * running time of your program should be proportional to log N in the worst
+ * case.
+ * Answer: Instead of searching based on powers of two (binary search), use
+ * Fibonacci numbers (which also grow exponentially). Maintain the current
+ * search range to be the interval [i, i + F k] and keep F k and F k–1 in two
+ * variables. At each step compute Fk–2 via subtraction, check element i +
+ * Fk–2 , and update the current range to either [i, i + Fk–2] or[i+Fk–2,
+ * i+Fk–2 +Fk–1].
  */
 public class FibonacciSearch {
-
+    
     private final int FI_SIZE = 20;
 
     public int fbSearch(int[] array, int target) {
