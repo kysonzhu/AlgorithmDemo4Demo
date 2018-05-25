20a5d926966fc0ea08e41e1c1d52f189ffdfa0d4
kyson <kyson@kysondeMacBook-Air.local>
2018-05-25 22:16:45 +0800

    [REV]ok

--- src/com/kyson/chapter1/section1/Practise_01_01_32.java
+++ src/com/kyson/chapter1/section1/Practise_01_01_32.java
@@ -7,39 +7,20 @@ public class Practise_01_01_32 {
 
     public static void main(String[] args) {
 
-        double[] a = {1.12, 1.22, 5.666, 9.0908, 8.902, 3.892, 10.782, 10.9,
-                10.19, 10.29, 20.1};
-        int N = 10;
-        double l = 1.09;
-        double r = 20.29;
-
-        // 组距
-        double classInterval = (r - l) / N;
-        // 对应到直方图中的组距
-        double intervalReact = 1 * 1.0 / N;
-
-        for (int i = 0; i < N; i++) {
-            // 每组数据的左值和右值
-            double tempLeft = l + classInterval * i;
-            double tempRight = l + classInterval * (i + 1);
-            // 每组数据的左值对应到直方图中的左值
-            double tempLeftReact = intervalReact * i;
-
-            /**
-             * 以下代码块是用于输入流在计算该组数下的频数
-             */
-            int tempNum = 0;
-            for (int j = 0; j < a.length; j++) {
-                if (a[j] > tempLeft && a[j] < tempRight) {
-                    tempNum++;
-                }
-            }
-            // 将频数转换为频率
-            double height = tempNum * 1.0 / (N * 2);
-            // 画图
-            StdDraw.filledRectangle(tempLeftReact, height, intervalReact,
-                    height);
-        }
+//        int N = 50;
+//        double[] a = new double[N];
+//        for (int i = 0; i < N; i++)
+//            a[i] = StdRandom.random();
+//
+//        for (int i = 0; i < N; i++) {
+//            double x = 1.0 * i / N;
+//            double y = a[i] / 2.0;
+//            double rw = 0.5 / N;
+//            double rh = a[i] / 2.0;
+//            System.out.println("x:"+x+",y:"+y+",rw:"+rw+",rh:"+rh);
+//            StdDraw.filledRectangle(x, y, rw, rh);
+//        }
+        StdDraw.filledRectangle(0.12,0.374366477461953,0.01,0.374366477461953);
 
     }
 }
