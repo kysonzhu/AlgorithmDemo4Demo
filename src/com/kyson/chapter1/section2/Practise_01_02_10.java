8b3f4ee2a7009e5790bdd6799c7d54600706ea2d
kyson <kyson@kysondeMacBook-Air.local>
2018-07-25 23:41:39 +0800

    [REV]Practise_01_02_10.java

--- src/com/kyson/chapter1/section2/Practise_01_02_10.java
+++ src/com/kyson/chapter1/section2/Practise_01_02_10.java
@@ -4,6 +4,18 @@ import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
+/*
+*
+* 1.2.10 编写一个类 VisualCounter，支持加一和减一操作。它的构造函数接受两个参数 N 和 max，
+* 其中 N 指定了操作的最大次数， max 指定了计数器的最大绝对值。作为副作用，用图像显示每次计数器变
+* 化后的值。
+
+1.2.10 Develop a class VisualCounter that allows both increment and decrement
+ operations. Take two arguments N and max in the constructor, where N specifies
+  the maximum number of operations and max specifies the maximum absolute value
+  for the counter. As a side effect, create a plot showing the value of the
+  counter each time its tally changes.
+* */
 public class Practise_01_02_10 {
 
     public static class VisualCounter {
