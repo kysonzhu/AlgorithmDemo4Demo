83b3b0dc9b3ded55f86c95d3a95729155309b7bb
程薇 <chengwei@chengweideMacBook-Air.local>
2018-08-20 10:52:35 +0800

    [REV]25

---
+++ src/com/kyson/chapter1/section3/Practise_01_03_25.java
@@ -0,0 +1,31 @@
+package com.kyson.chapter1.section3;
+
+public class Practise_01_03_25 {
+
+
+    public static class LinkedList {
+
+        private static class Node<Item> {
+            Item item;
+            Node next;
+        }
+
+
+        public void insertAfter(Node pos, Node node)
+        {
+            if (node == null || pos == node)
+                return;
+            node.next = pos.next;
+            pos.next = node;
+        }
+
+
+        public static void main(String[] args) {
+
+        }
+    }
+
+
+
+
+}
