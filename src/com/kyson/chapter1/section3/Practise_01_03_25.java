5acddf2cdf084c45fe20322055f2ed73656bf27a
kyson <kyson@kysondeMacBook-Air.local>
2018-08-20 23:26:37 +0800

    [REV]25,26,27

--- src/com/kyson/chapter1/section3/Practise_01_03_25.java
+++ src/com/kyson/chapter1/section3/Practise_01_03_25.java
@@ -1,5 +1,15 @@
 package com.kyson.chapter1.section3;
 
+/*
+*
+* 1.3.25 编写一个方法insertAfter()，接受两个链表结点作为参数，将第二结点插入链表并使之成为第一
+* 个结点的后续结点（如果两个参数为空则什么也不做）。
+
+1.3.25 Write a method insertAfter() that takes two linked-list Node arguments
+and inserts the second after the first on its list (and does nothing if either
+argument is null).
+*
+* */
 public class Practise_01_03_25 {
 
     public static class LinkedList {
